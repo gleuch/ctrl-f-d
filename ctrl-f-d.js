f31ff3299c362cf0f8c489aee248e05291716920
Greg Leuch <contact@gleuch.com>
2009-03-08 16:55:30 -0400

    converting to version 0.2. much much improvement on speed, and now supports colors.

--- ctrl-f-d.js
+++ ctrl-f-d.js
@@ -15,71 +15,70 @@
   Why Rush, you ask? Read this: http://mediamatters.org/items/200902130016
  
 */
- 
-var hide_bg = false;
- 
- 
+
+
+var hide_bg = true;
 var GM_JQ = document.createElement('script');
 GM_JQ.src = 'http://jquery.com/src/jquery-latest.js';
 GM_JQ.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(GM_JQ);
 function GM_wait() {if(typeof unsafeWindow.jQuery == 'undefined') { window.setTimeout(GM_wait,100); } else {unsafeWindow.jQuery.fn.reverse = function(){return this.pushStack(this.get().reverse(), arguments);}; $ = unsafeWindow.jQuery; $(document).ready(function() {ctrlfd();});}}
 Array.prototype.in_array = function(p_val, sensitive) {for(var i = 0, l = this.length; i < l; i++) {if ((sensitive && this[i] == p_val) || (!sensitive && this[i].toLowerCase() == p_val.toLowerCase())) {return true;}} return false;}
-function rgb2hex(rgb) {rgb = rgb.replace(/\s/g, "").replace(/^(rgb\()(\d+),(\d+),(\d+)(\))$/, "$2|$3|$4").split("|"); return "#" + hex(rgb[0]) + hex(rgb[1]) + hex(rgb[2]);}
+function rgb2hex(rgb) {rgb = rgb.replace(/\s/g, "").replace(/^(rgb\()(\d+),(\d+),(\d+)(\))$/, "$2|$3|$4").split("|"); return "#" + hex(rgb[0]) + hex(rgb[1]) + hex(rgb[2]);} 
 function hex(x) {var hexDigits = new Array("0", "1", "2", "3", "4", "5", "6", "7", "8","9", "A", "B", "C", "D", "E", "F"); return isNaN(x) ? "00" : hexDigits[(x-x%16)/16] + hexDigits[x%16];}
 GM_wait();
- 
+
 function ctrlfd() {
-  var rc = new RegExp("ctrlfd");
-  var rs = new RegExp("(>)$");
-  var re = new RegExp('^(!)');
-  var rb = new RegExp(" ", "ig");
-  var rx = new RegExp("^\s+|\s+$", "g");
-  var rt = new RegExp("^(script|textarea|style)");
-  setTimeout(function() {
-    $('object,embed,iframe').each(function() {
-      var r = $(this);
-      r.wrap(document.createElement('div'));
-      var s = $(this).parent();
-      s.css({width: r.width(), height: r.height()}).addClass('ctrlfd');
-      r.remove();
-    });
-  }, 1000);
- 
-  var x = $('body *');
-  x.each(function(n) {
-    var g = this.tagName.toLowerCase();
-    if (!(this.className == 'ctrlfd' || g == 'img' || g == 'script' || g == 'input' || g == 'textarea' || g == 'param' || g == 'embed' || g == 'object' || g == 'nobr' || g == 'select' || g == 'button' || g == 'br' || g == 'hr' || g == 'style' || g == 'meta' || g == 'link')) {// || a.in_array(this.tagName))) {
-      var r = $(this);
-      var t = r.html().replace(/\n/, "").split("<");
-      var tag_s = '<span class="ctrlfd" style="color: '+rgb2hex(r.css('color'))+' !important; background: '+ rgb2hex(r.css('color')) +' !important;">';
-      var tag_e = '</span>';
-      for (var i=0; i<t.length; i++) {
-        if (re.test(t[i]) || rt.test(t[i])) {
-          t[i] = '<'+t[i];
-        } else if (!(rs.test(t[i].replace(rx,"")))) {
-          var u = t[i].split(">");
-          if (u[1]) {
-            if (u[1].replace(rx,"") != '' && !(rc.test(u[0]))) u[1] = tag_s + u[1].replace(rx,"").replace(rb, tag_e +' '+ tag_s) + tag_e;
-            if (i > 0) u[0]='<'+u[0] +'>';
-          } else if (u[0].replace(rx,"") != '') {
-            u[0] = tag_s + u[0].replace(rx,"").replace(rb, tag_e+' '+tag_s) + tag_e;
-          }
-          t[i] = u.join("");
-        } else {
-          t[i] = '<'+t[i];
-        }
-      }
-      r.addClass('bg_ctrlfd').html(t.join(""));
-    }
-  });
-  $('img, input[type=image]').each(function() {
-    var r = $(this);
-    var w = r.width(); var h = r.height();
-    r.css({width: r.width(), height: r.height()}).attr('src', 'http://upload.wikimedia.org/wikipedia/commons/d/d2/Blank.png').width(w).height(h);
-  });
-  var s = document.createElement("style");
-  document.body.appendChild(s);
-  s.innerHTML = ".ctrlfd {display: inline !important; clear: none !important; float: none !important; "+ (hide_bg ? " background-image: none;" : "") +"} .bg_ctrlfd {"+ (hide_bg ? "background-image: none !important;" : "") +"} select, input, textarea, object, embed, img, button, hr {background: #000 !important; color: #000 !important; border-color: #000 !important;}";
-  if (hide_bg) $('body *, body, html').each(function() {$(this).addClass('bg_ctrlfd');});
+	var tb = '<span class="ctrlfd">';
+	var te = '</span>';
+	$('#guser a:eq(0)').html();
+	$.fn.ctrlfd = function(c) {
+		return this.filter(function() {if (this.nodeType == 1) {var g = this.tagName.toLowerCase(); return !(this.className == 'ctrlfd' || g == 'head' || g == 'img' || g == 'script');} else {return true;}}).each(function() {
+			if (this.nodeType == 3) {
+				if (this.nodeValue.replace(/\s/ig, '') != '') {
+					if (!c) c = '#000000';
+					var ntb = '<span class="ctrlfd" style="color: '+ c +'; background-color:'+c+';">';
+					$(this).after(ntb+ this.nodeValue.replace(/\s\s/ig, ' ').replace(/\n/ig, '').split(' ').join(te +' '+ ntb)+te);
+					this.nodeValue = '';
+				}
+			} else if (this.nodeType == 1) {
+				var c = rgb2hex($(this).css('color'));
+				if ($(this).children().length > 0) {
+					$(this).contents().ctrlfd(c);
+				} else if ($(this).children().length == 0) {
+					$(this).html(tb+ $(this).text() +te).addClass('ctrlfd').css({'background-color': c});
+				} else {
+					$(this).addClass('ctrlfd');
+				}
+			}
+		});
+	};
+	$('html').ctrlfd('#000000');
+
+	$(document).each(function() {
+		var r = this.title.split(" ");
+		for (var i=0; i<r.length; i++) if (!(/(\||\-)/ig).test(r[i])) r[i] = r[i].replace(/\S/ig, '-');
+		this.title = r.join(' ');
+	});
+
+	setTimeout(function() {
+		$('object,embed,iframe').each(function() {
+			var r = $(this);
+			r.wrap(document.createElement('div'));
+			var s = $(this).parent();
+			s.css({width: r.width(), height: r.height()}).addClass('ctrlfd');
+			r.remove();
+		});
+	}, 1000);
+
+	$('img, input[type=image]').each(function() {
+		var r = $(this);
+		var w = r.width(); var h = r.height();
+		r.css({width: r.width(), height: r.height()}).attr('src', 'http://upload.wikimedia.org/wikipedia/commons/d/d2/Blank.png').width(w).height(h);
+	});
+	var s = document.createElement("style");
+	document.body.appendChild(s);
+	s.innerHTML = ".ctrlfd {"+ (hide_bg ? "background-image: none !important;" : "") +"} .bg_ctrlfd {"+ (hide_bg ? "background-image: none !important;" : "") +"} select, input, textarea, object, embed, img, button, hr {background: #000 !important; color: #000 !important; border-color: #000 !important;}";
+
+	//if (hide_bg) $('body *, body, html').each(function() {$(this).addClass('bg_ctrlfd');});
 }
